11df194600729f4e1b5dc44e9d922e79d1901435
Arsene CLERY <ac139108@GHOSTIEM.iem>
2026-03-13 15:17:12 +0100

    ajout des premieres itérations godot

---
+++ robot.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+
+var speed = 3.0
+var player = null
+var chasing = false
+var target_position = Vector3.ZERO
+
+func _ready():
+	choose_random_position()
+
+func _physics_process(delta):
+
+	if chasing and player:
+		target_position = player.global_position
+
+	var direction = (target_position - global_position).normalized()
+
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+
+	move_and_slide()
+
+	if global_position.distance_to(target_position) < 1.5 and !chasing:
+		choose_random_position()
+
+func choose_random_position():
+	target_position = Vector3(
+		randf_range(-20,20),
+		global_position.y,
+		randf_range(-20,20)
+	)
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("player"):
+		player = body
+		chasing = true
