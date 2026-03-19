ae5d33eb7bb962fa64857126f5f40f1f9ea010de
Arsene CLERY <ac139108@MI105-09.iem>
2026-03-19 13:40:11 +0100

    icone dans le pc+boutons clickable

--- ONAUBE(OneNightAtUBE)/scenes_ordi/Desktop.gd
+++ ONAUBE(OneNightAtUBE)/scenes_ordi/Desktop.gd
@@ -2,3 +2,16 @@ extends Control
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	$VBoxContainer/TextureButton.grab_focus()
+
+
+func _on_texture_button_pressed() -> void:
+	print("hey")
+
+
+func _on_texture_button_2_pressed() -> void:
+	pass 
+
+
+func _on_texture_button_3_pressed() -> void:
+	pass
