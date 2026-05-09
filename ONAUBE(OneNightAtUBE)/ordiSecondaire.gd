6ff97f87aa7d746ab478dbc840fc16601a4193f5
JereMP4 <jeremythieblemont0@gmail.com>
2026-05-09 13:38:58 +0200

    Désormais, quand un mini-jeu est actif, l'écran est bleu

--- ONAUBE(OneNightAtUBE)/ordiSecondaire.gd
+++ ONAUBE(OneNightAtUBE)/ordiSecondaire.gd
@@ -1,57 +1,54 @@
 extends Node3D
 var player
 
+@export var id_MJ = 0
+@onready var minijeu = $MJ
+@onready var subMJ = $MJ/SubViewportContainer
+@onready var laptop_screen = get_node_or_null("laptop/Laptop/Laptop_Screen")
 
-@export var id_MJ=0
-@onready var minijeu=$MJ
-@onready var subMJ=$MJ/SubViewportContainer
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print("SCRIPT SUR :", get_path())
+	print("laptop_screen =", laptop_screen)
+	update_visual()
 
+func update_visual():
+	if not laptop_screen:
+		print("Laptop_Screen introuvable")
+		return
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	if id_MJ in GameState.activated_minigames:
+		laptop_screen.set_surface_override_material(2, null)
+
 func _process(delta: float) -> void:
+	update_visual()
 	if Input.is_action_just_pressed("pause") and GameState.on_pc and player:
-		
-		
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		GameState.on_pc=false
-		minijeu.visible=false
-		GameState.IGV=false
-		GameState.IGJ=false
-		GameState.IGC=false
+		GameState.on_pc = false
+		minijeu.visible = false
+		GameState.IGV = false
+		GameState.IGJ = false
+		GameState.IGC = false
 		
 	elif player and Input.is_action_just_pressed("interagir"):
 		interact()
-		
-		
-	
-		
+
 func interact():
-	GameState.on_pc=true
-	call_deferred("open_computer")
-	
+	if id_MJ in GameState.activated_minigames:
+		GameState.on_pc = true
+		call_deferred("open_computer")
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body.is_in_group("player") and id_MJ in GameState.activated_minigames:
-		player = body
-	else:
-		print("MJ non disponible")
-		
-		
-		
-
-
+	if body.is_in_group("player"):
+		if id_MJ in GameState.activated_minigames:
+			player = body
+		else:
+			print("MJ non disponible")
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		player=null
-		GameState.on_pc=false
-		
-		
+		player = null
+		GameState.on_pc = false
 
 func open_computer():
-	minijeu.visible=true
+	minijeu.visible = true
 	subMJ.open_minigame()
-	
